8a5792d077db353ea2c8d6790dfc6e06b0958fac
Diego Vega <00148816@uca.edu.sv>
2018-06-24 01:09:22 -0600

    Creacion del Panel

---
+++ src/Juego/PanelJuego.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Juego;
+
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author diego
+ */
+public class PanelJuego extends JPanel{
+    public static final int ANCHO=2090;
+    public static final int ALTO=990;
+    
+    public PanelJuego(){
+        
+    }
+}
