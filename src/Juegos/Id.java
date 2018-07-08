fbb1cd3669ff76b83ec657afbe42bda5ef0c81d5
Diego Vega <00148816@uca.edu.sv>
2018-07-08 13:51:39 -0600

    Nuevos controladores y clases

---
+++ src/Juegos/Id.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Juegos;
+
+/**
+ *
+ * @author diego
+ */
+public enum Id {
+	jugador, pared, hongo, enemigo, powerUp, pipe, moneda, Elenemigo1, enemigo2, bandera, planta,
+	estrella, trail, particula, flor, bolafuego
+}
+
