c2fd05c5af90f4c10d1f2b3d6ef924a4a67372c6
Bryanceren <00139817@uca.edu.sv>
2018-07-07 23:19:35 -0600

    commit

---
+++ src/game/LevelData.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game;
+
+/**
+ *
+ * @author Kriz
+ */
+public class LevelData {
+
+    public static final String[] LEVEL1 = new String[] {
+        "000000000000000000000000000000",
+        "000000000000000000000000000000",
+        "000000000000000000000000000000",
+        "000000000000000000000000000000",
+        "000000000000000000000000000000",
+        "000000000000000000000000000000",
+        "000000000000000000000000000000",
+        "000111000000000000000000000000",
+        "000000001110000000000000000000",
+        "000002000000011100002000000000",
+        "000001110000000000011100011000",
+        "111111110011110001111100111111"
+    };
+}
